52f54c6e631c454a99747afef7c64b9a91df145f
NasiadkaMaciej <maciej.nasiadka@gmail.com>
2022-03-14 10:10:32 +0100

    Working randomizing cards

--- app/src/main/java/com/example/oczko/MainActivity.java
+++ app/src/main/java/com/example/oczko/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
@@ -14,7 +15,8 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
     Random rand = new Random();
     ImageView card;
-    Integer points;
+    TextView pointTV;
+    Integer points = 0;
     Integer[] cardsId = {
             R.drawable.karo2,
             R.drawable.kier2,
@@ -68,75 +70,60 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.kieras,
             R.drawable.treflas,
             R.drawable.pikas};
+    Integer[] cardsPoints = {
+            2,
+            2,
+            2,
+            2,
+            3,
+            3,
+            3,
+            3,
+            4,
+            4,
+            4,
+            4,
+            5,
+            5,
+            5,
+            5,
+            6,
+            6,
+            6,
+            6,
+            7,
+            7,
+            7,
+            7,
+            8,
+            8,
+            8,
+            8,
+            9,
+            9,
+            9,
+            9,
+            10,
+            10,
+            10,
+            10,
+            2,
+            2,
+            2,
+            2,
+            3,
+            3,
+            3,
+            3,
+            4,
+            4,
+            4,
+            4,
+            11,
+            11,
+            11,
+            11};
 
-    int[][] naRodzaje = {
-            {
-                    R.drawable.karo2,
-                    R.drawable.kier2,
-                    R.drawable.trefl2,
-                    R.drawable.pik2
-            },
-            {       R.drawable.karo3,
-                    R.drawable.kier3,
-                    R.drawable.trefl3,
-                    R.drawable.pik3,
-            },
-            {       R.drawable.karo4,
-                    R.drawable.kier4,
-                    R.drawable.trefl4,
-                    R.drawable.pik4,
-            },
-            {       R.drawable.karo5,
-                    R.drawable.kier5,
-                    R.drawable.trefl5,
-                    R.drawable.pik5,
-            },
-            {       R.drawable.karo6,
-                    R.drawable.kier6,
-                    R.drawable.trefl6,
-                    R.drawable.pik6,
-            },
-            {       R.drawable.karo7,
-                    R.drawable.kier7,
-                    R.drawable.trefl7,
-                    R.drawable.pik7,
-            },
-            {       R.drawable.karo8,
-                    R.drawable.kier8,
-                    R.drawable.trefl8,
-                    R.drawable.pik8,
-            },
-            {       R.drawable.karo9,
-                    R.drawable.kier9,
-                    R.drawable.trefl9,
-                    R.drawable.pik9,
-            },
-            {       R.drawable.karo10,
-                    R.drawable.kier10,
-                    R.drawable.trefl10,
-                    R.drawable.pik10,
-            },
-            {       R.drawable.karowalet,
-                    R.drawable.kierwalet,
-                    R.drawable.treflwalet,
-                    R.drawable.pikwalet
-            },
-            {       R.drawable.karokrolowa,
-                    R.drawable.kierkrolowa,
-                    R.drawable.treflkrolowa,
-                    R.drawable.pikkrolowa,
-            },
-            {       R.drawable.karokrol,
-                    R.drawable.kierkrol,
-                    R.drawable.treflkrol,
-                    R.drawable.pikkrol,
-            },
-            {       R.drawable.karoas,
-                    R.drawable.kieras,
-                    R.drawable.treflas,
-                    R.drawable.pikas,
-            }
-    };
     ArrayList<Integer> set = new ArrayList<Integer>();
 
     @Override
@@ -144,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         card = findViewById(R.id.imageView);
+        pointTV = findViewById(R.id.pointsTV);
         set.clear();
         for(int i=0; i<cardsId.length;i++) {
             set.add(cardsId[i]);
@@ -154,9 +142,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void dobierz(View view) {
-        int x = (rand.nextInt(set.size()-1));
+        if (set.size() > 1) {
+            int x = (rand.nextInt(set.size() - 1));
+            for(int i=0; i<cardsId.length; i++){
+                if(cardsId[i]==set.get(x)){
+                    points = points + cardsPoints[i];
+                }
+            }
+            pointTV.setText(points);
+            card.setImageResource(set.get(x));
 
-        card.setImageResource(set.get(x));
-        set.remove(set.get(x));
+            set.remove(set.get(x));
+        }
     }
 }
+
